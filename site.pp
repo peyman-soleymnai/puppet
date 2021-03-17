803d6fe36aed4a07594dc2a6a3949b975fa2f442
root <root@server.itzgeek.local>
2021-03-17 15:49:58 +0330

    first commit

---
+++ site.pp
@@ -0,0 +1,29 @@
+
+
+node 'client1.puppet' {
+    class { 'linux':}
+
+}
+
+class linux {
+
+  $admintool = [ 'screen', 'nano', 'httpd']
+    
+  $httpservice = $osfamily ? {
+    'redhat' => 'httpd',
+    'debian' => 'apache2',
+    default => 'httpd',
+}
+
+
+     package { $admintool:
+        ensure => 'installed',
+            }
+                                      
+                                              
+     service { $httpservice:
+        ensure  => running,
+        require => Package['httpd'],
+        enable => true,
+             }
+}
